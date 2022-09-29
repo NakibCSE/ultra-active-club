5b69e243671e4df9e7949ab139cdb8707d5af4cb
nakibcse <nakkibcse@gmail.com>
2022-09-29 18:05:00 +0600

    all added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Activities from './Components/Activities/Activities';
 import Header from './Components/Header/Header';
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Blog from './Components/Blog/Blog';
 function App() {
   return (
     <div>
       <Header></Header>
+      <Activities></Activities>
+      <Blog></Blog>
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../asset/Nakib.jpg";
+import profile from "../../asset/Nakib .jpg";
 import "./Profile.css";
 const Profile = () => {
   return (
